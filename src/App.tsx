662f9a60a717dd3a3e7a216d5bd8e689180d7782
Luis Felipe Moreno Rondon <luisfelipe.moreno.contractor@bbva.com>
2025-09-17 19:11:43 -0500

    create backend folder and files

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { CoreAdmin, Resource } from "ra-core";
-
 import { Layout } from "./Layout";
-import { dataProvider } from "./dataProvider";
+import dataProvider from "./dataProvider";
 import { TodoList } from "./TodoList";
 
 export const App = () => (
   <CoreAdmin layout={Layout} dataProvider={dataProvider}>
-    <Resource name="todos" list={TodoList} />
+    <Resource
+      name="todos"
+      list={TodoList}
+    />
   </CoreAdmin>
 );
